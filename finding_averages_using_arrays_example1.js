4f4323398d26d1bae7720058d4397f69a235625c
anna mcd <ammcdevitt@users.noreply.github.com>
2016-11-06 09:55:39 -0500

    Create finding_averages_using_arrays_example1.js

---
+++ finding_averages_using_arrays_example1.js
@@ -0,0 +1,9 @@
+//finding average if there are elements in an array
+var numbersArray = [807, 187, 794, 282, 682, 998, 481, 681, 974, 291];
+
+var sum = 0;
+for (index = 0; index < grades.length; index++) {
+	sum += grades[index];
+}
+
+document.write(sum/grades.length);
