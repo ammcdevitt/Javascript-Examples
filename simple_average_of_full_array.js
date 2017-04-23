08d8428b0575cc9b1731e9dd12b4c30a4c77f9b9
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 15:27:33 -0600

    Update simple_average_of_full_array.js

--- simple_average_of_full_array.js
+++ simple_average_of_full_array.js
@@ -1,11 +1,17 @@
 //finding average if there are elements in an array
+
 var numbersArray = [807, 187, 794, 282, 682, 998, 481, 681, 974, 291];
+//declare variable called 'numbersArray' and assign elements to it
+//don't forget your semicolon!
 
 var sum = 0;
-//set counter to 0
+//declare var sum to be used as a counter, and set it to 0
 for (index = 0; index < numbersArray.length; index++) {
 //initializer; boolean condition; incrementor
+//as long as index is less than length of 'numbersArray' (which is 10), increment thru array
 	sum += numbersArray[index];
+	//add the given element to the total of the sum
 }
 
-document.write(sum/numbersArray.length);
+window.document.write(sum/numbersArray.length);
+	//displays to screen: (sum of all the elements in array)/(length of the array)
