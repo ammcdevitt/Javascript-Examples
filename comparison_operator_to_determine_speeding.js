cd2262268f2289497111db7378f0eaab009e58a4
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 14:31:31 -0600

    Update comparison_operator_to_determine_speeding.js

--- comparison_operator_to_determine_speeding.js
+++ comparison_operator_to_determine_speeding.js
@@ -5,6 +5,6 @@ if (speed >= 80) {
 } 
    //if speed is greater than 80, use console.log to print 'slow down, you tasmanian devil!'
 else {
-    console.log('yay! no tickets for you!');
+    console.log('yay, no tickets for you!');
 }
-   //otherwise (else), use console.log to print 'yay! no tickets for you!'
+   //otherwise (else), use console.log to print 'yay, no tickets for you!'
