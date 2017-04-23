7e7e473ae13bd22568201fee23e7538f94dc11e7
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 14:27:35 -0600

    Create comparison_operator_to_determine_speeding.js

---
+++ comparison_operator_to_determine_speeding.js
@@ -0,0 +1,11 @@
+//using comparison operator to determine whether you are speeding
+var speed = 65;
+   //check how fast you're driving
+if (speed >= 80) {
+    console.log('slow down, you tasmanian devil!'); 	
+} 
+    //if speed is greater than 80, use console.log to print "Slow down"
+else {
+    console.log('please drive safely');
+}
+    //otherwise (else), use console.log to print "please drive safely"
