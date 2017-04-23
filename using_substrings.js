9551123895d2c651b584a5a29457d2ab0f9a15ec
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 14:24:35 -0600

    Create using_substrings.js

---
+++ using_substrings.js
@@ -0,0 +1,8 @@
+var myName = "Anna";
+    //declare a variable myName and give it your name
+console.log(myName);
+    //use console.log to print out the myName variable; prints ‘Anna’
+console.log(myName.substring(0,2));
+     //change value of myName to just be the first 2 letters of your name; prints ‘An’
+console.log(myName);
+    //notice that the value of myName remains the full string, ‘Anna’
