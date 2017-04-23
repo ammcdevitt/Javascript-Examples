8162fc1a0b9ff2f2487039291142ebb4888630fe
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 14:50:22 -0600

    Update simple_average_of_full_array.js

--- simple_average_of_full_array.js
+++ simple_average_of_full_array.js
@@ -2,6 +2,7 @@
 var numbersArray = [807, 187, 794, 282, 682, 998, 481, 681, 974, 291];
 
 var sum = 0;
+//initializer; boolean condition; incrementor
 for (index = 0; index < grades.length; index++) {
 	sum += grades[index];
 }
