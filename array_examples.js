85cda99bb2f015ec8196251bec31bb72db47d7ef
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-24 11:28:05 -0600

    Create array_examples.js

---
+++ array_examples.js
@@ -0,0 +1,18 @@
+//ex1: declaring array using strings
+var foods = [‘bananas’, ‘apples’, ‘pizza’];
+
+//ex2: using APIs
+var images = document.getElementsByClassName[‘imgs’];
+//unsure how many images are in the document
+
+//ex3: using APIs
+var listItems = document.getElementsByTagName[‘li’];
+//declare array by making variable called listitems, and declare it by using document.getElementsByTagName and passing in ‘li’ in the parameter. Travels thru DOM & grab every element that has the li tag
+
+//ex4: using APIs
+var links= document.getElementsByTagName(‘a’); 
+    document.write(links[0]);
+//gets elements that have ‘a’ in the tag name. gets the first link in the code
+
+//ex5: don’t have to be same data type
+var info = [‘Anna’, 43593];
