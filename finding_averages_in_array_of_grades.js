0952f8cd9bbacc48f98c6a5ea58a5e050c68ef45
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-24 11:12:27 -0600

    Update finding_averages_in_array_of_grades.js

--- finding_averages_in_array_of_grades.js
+++ finding_averages_in_array_of_grades.js
@@ -5,31 +5,30 @@ var sum = 0;
 for (index = 0; index < grades.length; index++) {
 	sum += grades[index];
 }
-
 document.write(sum/grades.length);
 
 //example 2: finding average if there are no elements in array
-//you never want to divide by 0 in computer science
+//you never want to divide by 0 in CS
 var grades= [];
 
 var sum = 0;
 if (grades.length > 0){
 	for (index = 0; index < grades.length; index++){
-  	//before finding the average, computer checks length of array to make sure that it's greater than 0
+  	//before finding average, computer checks length of array to make sure it's greater than 0
 		sum += grades[index];
 	}  
   	document.write(sum/grades.length);
 }
 else {
 	document.write("Empty Array");
+	//prints out 'Empty Array' if there are no elements in array
 }
-//just prints out empty array if there are no elements in array
 
 //example 3: array average with undefined (empty) elements
 var grades= [2, 5, , , 9, 8, , 8];
 var sum = 0;
 var count = 0;
-//declaring count allows the sum to be divided by ONLY the correct numbers (5), instead of the length of the array (8)
+//declaring count allows sum to be divided by the correct total numbers (5), instead of the length of array (8)
 
 if (grades.length > 0){
 	for (index = 0; index < grades.length; index++){
