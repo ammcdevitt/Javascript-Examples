59705c270b6990a3e4e830c61cc7473559908e24
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-24 11:27:02 -0600

    Update global_v_local_scope.js

--- global_v_local_scope.js
+++ global_v_local_scope.js
@@ -1,12 +1,15 @@
 //GLOBAL SCOPE
-var globalVar = "hello";
+var globalVar = 'hello';
+//variable 'globalVar' can be accessed anywhere, even inside the function 'foo'
 var foo = function() {
     console.log(globalVar);  
-    // prints "hello". the variable globalVar can be accessed anywhere, even inside the function foo
+    //displays 'hello'
 }
 
-//LOCAL SCOPE: variable localVar only exists inside the function bar. printing to the console must occur within the function, otherwise, it will error.
+//LOCAL SCOPE
 var bar = function() {
     var localVar = "howdy";
+    //variable 'localVar' only exists inside the function 'bar'
     console.log(localVar); 
+    //displaying to console must occur within the function, otherwise, it will error
 }
