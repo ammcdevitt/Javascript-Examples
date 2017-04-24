e58df9ca015dcf08ba671480b9d0c56256274636
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-24 11:31:57 -0600

    Update array_examples.js

--- array_examples.js
+++ array_examples.js
@@ -1,18 +1,15 @@
 //ex1: declaring array using strings
 var foods = [‘bananas’, ‘apples’, ‘pizza’];
+//ex2: but don’t have to be same data type
+var info = [‘Anna’, 43593];
 
-//ex2: using APIs
+//ex3: access specific Class Name in DOM
 var images = document.getElementsByClassName[‘imgs’];
 //unsure how many images are in the document
 
-//ex3: using APIs
+//ex4: access specific Tag Name in DOM
 var listItems = document.getElementsByTagName[‘li’];
-//declare array by making variable called listitems, and declare it by using document.getElementsByTagName and passing in ‘li’ in the parameter. Travels thru DOM & grab every element that has the li tag
-
-//ex4: using APIs
-var links= document.getElementsByTagName(‘a’); 
-    document.write(links[0]);
-//gets elements that have ‘a’ in the tag name. gets the first link in the code
-
-//ex5: don’t have to be same data type
-var info = [‘Anna’, 43593];
+//declares array by making variable called listitems,
+//then declares it by using document.getElementsByTagName,
+//then passes ‘li’ into parameter, and
+//travels thru DOM & grab every element that has li tag
