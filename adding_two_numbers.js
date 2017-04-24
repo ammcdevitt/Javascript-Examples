efaa2a2b94af1ac007f8310ddfd09543172dfea6
anna mcd <ammcdevitt@users.noreply.github.com>
2017-04-23 18:08:00 -0600

    Create adding_two_numbers.js

---
+++ adding_two_numbers.js
@@ -0,0 +1,16 @@
+//function that adds 2 numbers
+
+//initialize variables var1 and var2 to empty elements
+var val1 = "";
+var val2 = "";
+
+//declare function with keyword and the add function name
+//pass in the 2 variables, val1 and val2, or else the func won't know what to add!
+function add(val1, val2) {
+  return var1 + var2;
+  }
+
+add(1,2);   
+//call the add func and pass in the 2 numbers (arguments) that you want to add
+//in this case, 1 and 2 are the arguments to val1 and val2, respectively
+  
